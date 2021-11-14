01534796f3610f5c662dea56cebf9f02dd2b1fec
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2021-11-14 10:25:57 +0330

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use std::net::TcpListener;
+
+const PORT: &str = "7878";
+const ADDRESS: &str = "127.0.0.1";
+
+fn main() {
+    let listener = TcpListener
+    ::bind(format!("{}:{}", ADDRESS, PORT)).unwrap();
+    println!("Starting server at {}:{} ...", ADDRESS, PORT);
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("Connection Stablished: {:?}",stream)
+    }
+}
