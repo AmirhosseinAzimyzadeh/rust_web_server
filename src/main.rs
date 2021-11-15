dd41c4ced6063158332827b1838930704316a88b
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2021-11-15 08:49:58 +0330

    trying to add thread pool data structure

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::net::TcpListener;
 mod connection;
+mod thread_pool;
+use thread_pool::ThreadPool;
+use std::thread;
 
 const PORT: &str = "7878";
 const ADDRESS: &str = "127.0.0.1";
@@ -9,8 +12,11 @@ fn main() {
     ::bind(format!("{}:{}", ADDRESS, PORT)).unwrap();
     println!("Starting server at {}:{} ...", ADDRESS, PORT);
 
+    // creating thread pool
+    let pool = ThreadPool::new(10);
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        connection::handler(stream);
+        pool.run();
     }
 }
