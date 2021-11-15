9aeefaa2d3502d34ddd83df3df2e3202e74b1569
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2021-11-15 07:48:19 +0330

    add responses

--- src/connection.rs
+++ src/connection.rs
@@ -1,7 +1,15 @@
 use std::{io::Read, net::TcpStream};
+use std::io::prelude::*;
+#[path = "./responses.rs"] mod responses;
 
 pub fn handler(mut stream: TcpStream) {
   let mut buffer = [0; 1024];
   stream.read(&mut buffer).unwrap();
+
   println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+  let response = responses::create_response(responses::Response::OK);
+
+  stream.write(response.as_bytes()).unwrap();
+  stream.flush().unwrap();
 }
